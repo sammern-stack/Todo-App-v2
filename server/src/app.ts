c1491704f6885aed02e7f4964d979438e41d93aa
sammern <samreactdev@gmail.com>
2026-05-28 00:07:02 +0300

    refactor(app): put errorHandler last on the app.ts and added section for routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,14 +3,25 @@
 //—————————————————————————————————————————————————————————————————
 
 import express from "express";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 // Initialize Express app
-const app = express()
+const app = express();
 
 //—————————————————————————————————————————————————————————————————
 // Middleware
 //—————————————————————————————————————————————————————————————————
 
-app.use(express.json())
+app.use(express.json());
+
+//—————————————————————————————————————————————————————————————————
+// Routes
+//—————————————————————————————————————————————————————————————————
+
+// routes will go here
+
+// Handle errors here
+// Always last so it catches all errors
+app.use(errorHandler);
 
 export default app;
