f852cb0374115a14d2b00f349072050b67cfa1ef
sammern <samreactdev@gmail.com>
2026-05-28 23:56:59 +0300

    feat(component): create a CreateTodo component for creating and editing todos

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { ThemeSwitch } from "./ThemeSwitch/ThemeSwitch";
 export { BackgroundImage } from "./BackgroundImage/BackgroundImage";
 export { Button } from "./Button/Button";
+export { CreateTodo } from "./CreateTodo/CreateTodo";
