6cca69d390f7998ed1ae5d608a4b1e679630c2c3
sammern <samreactdev@gmail.com>
2026-05-28 10:24:11 +0300

    feat(util): create an apiResonse utility to handle sending a succes/error responces

---
+++ server/src/utils/apiResponse.ts
@@ -0,0 +1,36 @@
+//—————————————————————————————————————————————————————————————————
+// Imports
+//—————————————————————————————————————————————————————————————————
+
+import type { Response } from "express";
+
+//—————————————————————————————————————————————————————————————————
+// Types
+//—————————————————————————————————————————————————————————————————
+
+type TSendSuccess = (
+  res: Response,
+  data: unknown,
+  code: number,
+  meta?: Record<string, unknown>,
+  message?: string,
+) => void;
+
+type TSendError = (res: Response, message: string, code: number) => void;
+
+//—————————————————————————————————————————————————————————————————
+// Api Response Utilities
+//—————————————————————————————————————————————————————————————————
+
+export const sendSuccess: TSendSuccess = (res, data, code, meta, message) => {
+  res.status(code ?? 200).json({
+    ok: true,
+    data,
+    ...(meta && { meta }),
+    ...(message && { message }),
+  });
+};
+
+export const sendError: TSendError = (res, message, code) => {
+  res.status(code ?? 500).json({ ok: false, message });
+};
