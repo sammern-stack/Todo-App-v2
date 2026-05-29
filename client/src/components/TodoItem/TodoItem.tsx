d77640d502aad86b941bde665a00d4fff6d9eca5
sammern <samreactdev@gmail.com>
2026-05-29 14:34:12 +0300

    feat(todos): create an todo item and list components

---
+++ client/src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,36 @@
+//—————————————————————————————————————————————————————————————————
+// Imports
+//—————————————————————————————————————————————————————————————————
+
+import { Button } from "../Button/Button";
+import type { ITodo } from "../../types";
+import "./TodoItem.scss";
+
+//—————————————————————————————————————————————————————————————————
+// Types
+//—————————————————————————————————————————————————————————————————
+
+interface TodoItemProps {
+  todo: ITodo;
+}
+
+//—————————————————————————————————————————————————————————————————
+//* Todo item Component
+//—————————————————————————————————————————————————————————————————
+
+export const TodoItem = ({ todo }: TodoItemProps) => {
+  return (
+    <div className="todo">
+      <div className="todo__info">
+        <Button stage={todo.stage} isTodo/>
+        {todo.title}
+      </div>
+
+      <div className="todo__actions">
+        <div className="todo__edit">E</div>
+
+        <div className="todo-delete">D</div>
+      </div>
+    </div>
+  );
+};
