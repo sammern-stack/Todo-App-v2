f782411d5446fb322999730e097f3d97a721f27d
sammern <samreactdev@gmail.com>
2026-05-29 14:33:39 +0300

    refactor(button): update to handle todo btn differently from add btn

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,9 +1,34 @@
+//—————————————————————————————————————————————————————————————————
+// Imports
+//—————————————————————————————————————————————————————————————————
+
+import type { TStage } from "../../types";
+import check from "../../assets/icon-check.svg";
 import "./Button.scss";
 
-export const Button = () => {
+//—————————————————————————————————————————————————————————————————
+// Types
+//—————————————————————————————————————————————————————————————————
+
+interface ButtonProps {
+  isTodo?: boolean;
+  stage?: TStage;
+  isInput?: boolean;
+}
+
+//—————————————————————————————————————————————————————————————————
+// Button Component
+//—————————————————————————————————————————————————————————————————
+
+export const Button = ({ isTodo, stage, isInput }: ButtonProps) => {
+  const isDone = isTodo && stage === "complete";
+  const isChecked = isDone ? "button--checked" : "";
+  const showBody = stage === "incomplete" || isInput;
+
   return (
-    <button className="button">
-      <div className="button__body"></div>
+    <button className={`button ${isChecked}`}>
+      {isDone && <img src={check} alt="checkmark for completed todo" />}
+      {showBody && <div className="button__body"></div>}
     </button>
   );
 };
