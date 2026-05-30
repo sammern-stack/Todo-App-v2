bda52c4b150f24d43f3bb9922e4ea51074c38edf
sammern <samreactdev@gmail.com>
2026-05-30 14:51:20 +0300

    refactor button component with update render logic and discriminated types

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -2,7 +2,6 @@
 // Imports
 //—————————————————————————————————————————————————————————————————
 
-import type { TStage } from "../../types";
 import check from "../../assets/icon-check.svg";
 import "./Button.scss";
 
@@ -10,25 +9,41 @@ import "./Button.scss";
 // Types
 //—————————————————————————————————————————————————————————————————
 
-interface ButtonProps {
-  isTodo?: boolean;
-  stage?: TStage;
-  isInput?: boolean;
-}
+type BaseBtnProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+type ButtonProps =
+  | (BaseBtnProps & { role: "create" })
+  | (BaseBtnProps & { role: "todo"; isChecked: boolean });
 
 //—————————————————————————————————————————————————————————————————
 // Button Component
 //—————————————————————————————————————————————————————————————————
 
-export const Button = ({ isTodo, stage, isInput }: ButtonProps) => {
-  const isDone = isTodo && stage === "complete";
-  const isChecked = isDone ? "button--checked" : "";
-  const showBody = stage === "incomplete" || isInput;
+export const Button = (props: ButtonProps) => {
+  const renderContent = () => {
+    if (props.role === "create" || !props.isChecked)
+      return <div className="button--empty-state"></div>;
+
+    return <img src={check} alt="checkmark for completed todo" />;
+  };
+
+  const isTodoChecked = () =>
+    props.role === "todo" && props.isChecked ? "checked" : "notChecked";
+
+  const hasHover = () =>
+    (props.role === "todo" && !props.isChecked) || props.role === "create"
+      ? "hasHover"
+      : "noHover";
+
+  const classes = [
+    "button",
+    `button--${isTodoChecked()}`,
+    `button--${hasHover()}`,
+  ].join(" ");
 
   return (
-    <button className={`button ${isChecked}`}>
-      {isDone && <img src={check} alt="checkmark for completed todo" />}
-      {showBody && <div className="button__body"></div>}
+    <button className={classes} {...props}>
+      {renderContent()}
     </button>
   );
 };
