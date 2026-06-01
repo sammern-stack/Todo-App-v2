055b84252ba721abf930b7fec77e73a912f85924
sammern <samreactdev@gmail.com>
2026-06-01 14:32:00 +0300

    feat(axios): create axios instance and a helper function to handle axios requests

---
+++ client/src/api/axios.ts
@@ -0,0 +1,25 @@
+//—————————————————————————————————————————————————————————————————
+// Imports
+//—————————————————————————————————————————————————————————————————
+
+import axios from "axios";
+import type { AxiosFn, RequestFn } from "../types";
+
+//—————————————————————————————————————————————————————————————————
+// Axios Utility function
+//—————————————————————————————————————————————————————————————————
+
+export const api = axios.create({ baseURL: "http://localhost:3001/api/todos" });
+
+export const apiCall = async <T>(fn: AxiosFn<T>): RequestFn<T> => {
+  try {
+    const { data } = await fn();
+    return { ...data };
+  } catch (err) {
+    console.log("Error:", err);
+    return {
+      ok: false,
+      message: `An unexpected error occurred: ${err}`,
+    };
+  }
+};
