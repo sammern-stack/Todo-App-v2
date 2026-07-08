527484ccdbcc689c6131d43454b2287007975530
sammern <samreactdev@gmail.com>
2026-07-08 16:25:44 +0300

    feat: create a queryClient.ts and use it in my main.tsx with QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,19 @@
+import "@/shared/styles/globals.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-
-import "@/shared/styles/globals.scss";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import { client } from "@/shared/lib/queryClient.ts";
 import App from "./App.tsx";
+
 
 const rootEl = document.getElementById("root")!;
 createRoot(rootEl).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
