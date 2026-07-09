fc0ed583f2956355299338cb14d4c20c50d1b143
sammern <samreactdev@gmail.com>
2026-07-09 23:37:21 +0300

    refactor: simplify TodoActions component by using a paragraph for items left and extracting clearTodos handler

--- client/src/features/Todos/components/TodoActions/TodoActions.tsx
+++ client/src/features/Todos/components/TodoActions/TodoActions.tsx
@@ -5,19 +5,20 @@ import { TodoFilters } from "../TodoFilters/TodoFilters";
 export const TodoActions = () => {
   const { data: todos = [] } = useTodos();
   const { mutate: clearTodos } = useClearTodos();
+  const handleClearTodos = () => clearTodos();
   const todosLeft = todos.filter((todo) => todo.stage === "incomplete").length;
 
   return (
     <div className={styles.todosActions}>
-      <div className={styles["todosActions__items-left"]}>
+      <p className={styles["todosActions__items-left"]}>
         {todosLeft} items left
-      </div>
+      </p>
 
       <TodoFilters />
 
       <button
         className={styles["todosActions__clear-all"]}
-        onClick={() => clearTodos()}
+        onClick={handleClearTodos}
       >
         Clear completed
       </button>
