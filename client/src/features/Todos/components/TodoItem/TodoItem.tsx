930e65bddd0bc62a54349cd0c7f1e27af6f21a6f
sammern <samreactdev@gmail.com>
2026-07-10 12:08:06 +0300

    feat: add bounce animations for error and success states in CreateTodo; enhance TodoItem with delete animation

--- client/src/features/Todos/components/TodoItem/TodoItem.tsx
+++ client/src/features/Todos/components/TodoItem/TodoItem.tsx
@@ -1,10 +1,12 @@
 import styles from "./TodoItem.module.scss";
+import { useState } from "react";
 import { useDeleteTodo, useUpdateTodo } from "@/features/Todos";
 import { Button } from "@/shared/components";
 import CrossIcon from "@/assets/icon-cross.svg?react";
 import type { TodoSchema } from "@/shared/types/todo.types";
 
 export const TodoItem = ({ todo }: { todo: TodoSchema }) => {
+  const [deleting, setDeleting] = useState(false);
   const { mutate: deleteTodo } = useDeleteTodo();
   const { mutate: updateTodo } = useUpdateTodo();
 
@@ -17,19 +19,29 @@ export const TodoItem = ({ todo }: { todo: TodoSchema }) => {
         stage: isCompleted ? "incomplete" : "completed",
       },
     });
-  const handleDelete = () => deleteTodo(todo._id);
+  const handleDelete = () => {
+    setDeleting(true);
+    setTimeout(() => {
+      deleteTodo(todo._id);
+    }, 500);
+  };
 
   const todoClasses = [
     styles.todo,
     isCompleted ? styles["todo--completed"] : "",
+    deleting ? styles["todo--deleting"] : "",
   ].join(" ");
 
   return (
-    <li className={todoClasses} onClick={handleToggleState}>
+    <li className={todoClasses}>
       <Button role="todo" isChecked={isCompleted} onClick={handleToggleState} />
-      <p className={styles.todo__title}>{todo.title}</p>
+      <p className={styles.todo__title} onClick={handleToggleState}>
+        {todo.title}
+      </p>
       <div className={styles.todo__actions}>
-        <CrossIcon className={styles.todo__delete} onClick={handleDelete} />
+        <div className={styles.todo__delete}>
+          <CrossIcon onClick={handleDelete} />
+        </div>
       </div>
     </li>
   );
